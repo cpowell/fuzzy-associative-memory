171012b3ff7953b5b12acc473e231371e9b1cd1a
Chris Powell <cpowell@prylis.com>
2013-06-24 09:53:47 -0700

    Tidying.

--- bin/runner.rb
+++ bin/runner.rb
@@ -1,7 +1,9 @@
 $:.push File.expand_path('../../lib/', __FILE__)
 
-flv = FuzzyLinguisticVariable.new("Distance To Target")
-flv.add_set LeftShoulder.new(25, 25, 125)
-flv.add_set Triangle.new(150, 125, 150)
-flv.add_set Triangle.new(300, 150, 200)
+temperature_in = FuzzyRuleset.new("Temperature In")
+temperature_in.add_rule LeftShoulder.new(-1, 0, 50)
+temperature_in.add_rule Triangle.new(0, 55, 65)
+temperature_in.add_rule Triangle.new(60, 65, 70)
+temperature_in.add_rule Triangle.new(65, 75, 85)
+temperature_in.add_rule RightShoulder.new(80, 90, 100)
 
--- lib/fuzzy_ruleset.rb
+++ lib/fuzzy_ruleset.rb
@@ -1,19 +1,19 @@
-class FuzzyLinguisticVariable
+class FuzzyRuleset
 
-  attr_reader @member_sets
+  attr_reader @rules
   attr_reader @min_range
   attr_reader @max_range
   attr_reader @name
 
   def initialize(name)
     @name=name
-    @member_sets = []
+    @rules = []
     @min_range=0
     @max_range=0
   end
 
-  def add_set(fuzzy_set)
-    @member_sets << fuzzy_set
+  def add_rule(fuzzy_set)
+    @rules << fuzzy_set
   end
 
 
