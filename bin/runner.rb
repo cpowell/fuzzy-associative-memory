ad03286209703d79697492d4aca24123bd995475
Chris Powell <cpowell@prylis.com>
2013-06-24 10:20:03 -0700

    A basic runner to get a picture of how things are shaping up. Tidying.

--- bin/runner.rb
+++ bin/runner.rb
@@ -1,4 +1,10 @@
+#!/usr/bin/env ruby
+
 $:.push File.expand_path('../../lib/', __FILE__)
+require 'fuzzy_ruleset'
+require 'triangle'
+require 'left_shoulder'
+require 'right_shoulder'
 
 temperature_in = FuzzyRuleset.new("Temperature In")
 temperature_in.add_rule LeftShoulder.new(-1, 0, 50)
@@ -6,4 +12,11 @@ temperature_in.add_rule Triangle.new(0, 55, 65)
 temperature_in.add_rule Triangle.new(60, 65, 70)
 temperature_in.add_rule Triangle.new(65, 75, 85)
 temperature_in.add_rule RightShoulder.new(80, 90, 100)
+
+temperature_in.result_for(65)
+temperature_in.result_for(64)
+temperature_in.result_for(63)
+temperature_in.result_for(62)
+temperature_in.result_for(61)
+temperature_in.result_for(60)
 
--- lib/fuzzy_ruleset.rb
+++ lib/fuzzy_ruleset.rb
@@ -1,19 +1,23 @@
 class FuzzyRuleset
-
-  attr_reader @rules
-  attr_reader @min_range
-  attr_reader @max_range
-  attr_reader @name
+  attr_reader :rules
+  attr_reader :name
 
   def initialize(name)
-    @name=name
-    @rules = []
-    @min_range=0
-    @max_range=0
+    @name      = name
+    @rules     = []
   end
 
   def add_rule(fuzzy_set)
     @rules << fuzzy_set
+  end
+
+  def result_for(value)
+    @doms = Array.new
+    @rules.each do |rule|
+      @doms << rule.dom(value)
+    end
+
+    puts @doms.join(", ")
   end
 
 
